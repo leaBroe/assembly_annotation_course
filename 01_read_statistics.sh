463328cad7e75f6d45f3fbd6a97b526877b0c9b8
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2023-09-21 16:43:18 +0200

    Improved Reproducibility of scripts

--- 01_read_statistics.sh
+++ 01_read_statistics.sh
@@ -1,39 +1,47 @@
 #!/usr/bin/env bash
 
+# Set environment variables and paths
+WORKING_DIR="/data/users/lbroennimann/assembly_annotation_course"
+OUTPUT_DIR="$WORKING_DIR/read_QC/fastqc"
+LOG_DIR="$OUTPUT_DIR/logs"
+PARTICIPANT_DIR="$WORKING_DIR/participant_3"
+EMAIL=lea.broennimann@students.unibe.ch
+
+# Create output and log directories if they don't exist
+mkdir -p "$OUTPUT_DIR/illumina"
+mkdir -p "$OUTPUT_DIR/pacbio"
+mkdir -p "$OUTPUT_DIR/RNAseq"
+mkdir -p "$LOG_DIR"
+
+# Define output and error log file paths
+OUTPUT_LOG="$LOG_DIR/output_fastqc_%j.o"
+ERROR_LOG="$LOG_DIR/error_fastqc_%j.e"
+
+# SBATCH parameters for job
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4000M
 #SBATCH --time=03:00:00
 #SBATCH --job-name=fastqc
-#SBATCH --mail-user=lea.broennimann@students.unibe.ch
+#SBATCH --mail-user=$EMAIL
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/output_error/output_fastqc_%j.o
-#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/output_error/error_fastqc_%j.e
+#SBATCH --output=$OUTPUT_LOG
+#SBATCH --error=$ERROR_LOG
 #SBATCH --partition=pall
 
+# Load FastQC module
+module load UHTS/Quality_control/fastqc/0.11.9
 
-cd /data/users/lbroennimann/assembly_annotation_course
+# Perform FastQC on Illumina files
+cd "$PARTICIPANT_DIR/Illumina"
+fastqc -o "$OUTPUT_DIR/illumina" *fastq.gz
 
-# load fastqc module
-module load UHTS/Quality_control/fastqc/0.11.9;
+# Perform FastQC on PacBio files
+cd "$PARTICIPANT_DIR/pacbio"
+fastqc -o "$OUTPUT_DIR/pacbio" *fastq.gz
 
-# define and change to directory where the fastq files are
-#READS_DIR = /data/users/lbroennimann/assembly_annotation_course/participant_3
-
-
-# fastqc on illumina files
-cd /data/users/lbroennimann/assembly_annotation_course/participant_3/Illumina
-mkdir /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/illumina
-fastqc -o /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/illumina *fastq.gz
-
-# fastqc on pacbio files
-cd /data/users/lbroennimann/assembly_annotation_course/participant_3/pacbio
-mkdir /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/pacbio
-fastqc -o /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/pacbio *fastq.gz
-
-# fastqc on RNAseq files
-cd /data/users/lbroennimann/assembly_annotation_course/participant_3/RNAseq
-mkdir /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/RNAseq
-fastqc -o /data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/RNAseq *fastq.gz
+# Perform FastQC on RNAseq files
+cd "$PARTICIPANT_DIR/RNAseq"
+fastqc -o "$OUTPUT_DIR/RNAseq" *fastq.gz
 
 
 
--- 02_perform_k_mer_counting.sh
+++ 02_perform_k_mer_counting.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=40000M
-#SBATCH --time=04:00:00
-#SBATCH --job-name=kmer_counting
-#SBATCH --mail-user=lea.broennimann@students.unibe.ch
-#SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/k_mer_counts/output_error/output_jellyfish_%j.o
-#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/k_mer_counts/output_error/error_jellyfish_%j.e
+# Set environment variables and paths
+WORKDIR="/data/users/lbroennimann/assembly_annotation_course"
+READ_DIR="$WORKDIR/participant_3"
+OUTPUT_DIR="$WORKDIR/read_QC/kmer_counts"
+LOG_DIR="$OUTPUT_DIR/logs"
+EMAIL="lea.broennimann@students.unibe.ch"
+
+# Create output and log directories if they don't exist
+mkdir -p "$OUTPUT_DIR"
+mkdir -p "$LOG_DIR"
+
+# SBATCH parameters for the job
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8000M
+#SBATCH --time=05:00:00
+#SBATCH --job-name=kmercount
+#SBATCH --mail-user="$EMAIL"
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/read_QC/kmer_counts/logs/02-kmercount_%j.o
+#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/read_QC/kmer_counts/logs/02-kmercount_%j.e"
 #SBATCH --partition=pall
 
-module add UHTS/Analysis/jellyfish/2.3.0
+# Load Jellyfish module
+module load UHTS/Analysis/jellyfish/2.3.0
 
-# k-mer = 14
+# Perform k-mer counting on Illumina files
+IN="$READ_DIR/Illumina"
+OUT="$OUTPUT_DIR"
 
-#jellyfish count -C -m 14 -s 1000000000 -t 10 *.fastq -o reads.jf
+FILES=("$IN"/*.fastq.gz)
+SEQS=("${FILES[@]}")
 
-cd /data/users/lbroennimann/assembly_annotation_course/participant_3/Illumina
+cd "$OUT"
 
-jellyfish count -C -m 14 -s 5G -t 4 <(zcat *.fastq.gz) -o reads.jf
+# Run Jellyfish count and histo
+jellyfish count -C -m 19 -s 5G -t 4 <(zcat "${SEQS[0]}") <(zcat "${SEQS[1]}") -o ill_reads.jf
+#jellyfish histo -t 10 ill_reads.jf > ill_reads.histo
 
