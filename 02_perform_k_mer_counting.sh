4ba919479222ca30bbb8d67470da0b52cb223a5b
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2023-09-22 19:06:23 +0200

    improved k mer counting

--- 02_perform_k_mer_counting.sh
+++ 02_perform_k_mer_counting.sh
@@ -6,15 +6,15 @@
 #SBATCH --job-name=kmercount
 #SBATCH --mail-user=lea.broennimann@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/lbroennimann/02-kmercount_%j.o
-#SBATCH --error=/data/users/lbroennimann/02-kmercount_%j.e
+#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/read_QC/kmer_counts/logs/02-kmercount_%j.o
+#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/read_QC/kmer_counts/logs/02-kmercount_%j.e
 #SBATCH --partition=pall
 
 module load UHTS/Analysis/jellyfish/2.3.0
 
 WORKDIR="/data/users/lbroennimann/assembly_annotation_course"
 READ_DIR="/data/users/lbroennimann/assembly_annotation_course/participant_3"
-OUTPUT_DIR="/data/users/jackermann/assembly_annotation_course/read_QC/kmer_counting"
+OUTPUT_DIR="/data/users/lbroennimann/assembly_annotation_course/read_QC/kmer_counts"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
@@ -41,16 +41,16 @@ illumina_output_prefix="${OUTPUT_DIR}/ill_reads"
 run_kmer_counting "$illumina_input_dir" "$illumina_output_prefix"
 
 # PacBio
-pacbio_input_dir="${READ_DIR}/pacbio" # Update the directory path accordingly
+pacbio_input_dir="${READ_DIR}/pacbio" 
 pacbio_output_prefix="${OUTPUT_DIR}/pacbio_reads"
 run_kmer_counting "$pacbio_input_dir" "$pacbio_output_prefix"
 
 # RNAseq
-rnaseq_input_dir="${READ_DIR}/RNAseq" # Update the directory path accordingly
+rnaseq_input_dir="${READ_DIR}/RNAseq" 
 rnaseq_output_prefix="${OUTPUT_DIR}/rnaseq_reads"
 run_kmer_counting "$rnaseq_input_dir" "$rnaseq_output_prefix"
 
-# run jellyfish histo 
-jellyfish histo -t 10 ill_reads.jf > ill_reads.histo
-jellyfish histo -t 10 pacbio_reads.jf > pacbio_reads.histo
-jellyfish histo -t 10 rnaseq_reads.jf > rnaseq_reads.histo
+# Run jellyfish histo
+jellyfish histo -t 10 "${illumina_output_prefix}_${SAMPLENAME}.jf" > "${illumina_output_prefix}_${SAMPLENAME}.histo"
+jellyfish histo -t 10 "${pacbio_output_prefix}_${SAMPLENAME}.jf" > "${pacbio_output_prefix}_${SAMPLENAME}.histo"
+jellyfish histo -t 10 "${rnaseq_output_prefix}_${SAMPLENAME}.jf" > "${rnaseq_output_prefix}_${SAMPLENAME}.histo"
