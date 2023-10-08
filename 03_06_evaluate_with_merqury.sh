c3db7e9b310cf3ff85d5c9a1b6cdf5a230358e17
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2023-10-08 20:55:49 +0200

    made merqury work

--- 03_06_evaluate_with_merqury.sh
+++ 03_06_evaluate_with_merqury.sh
@@ -4,10 +4,10 @@
 #SBATCH --mem=64G
 #SBATCH --time=08:00:00
 #SBATCH --job-name=merqury_evaluation
-#SBATCH --mail-user=sabrina.rasch@students.unibe.ch
+#SBATCH --mail-user=lea.broennimann@students.unibe.ch
 #SBATCH --mail-type=begin,end,fail
-#SBATCH --output=/data/users/srasch/assembly_course/Output/output_merqury_evaluation_%j.o
-#SBATCH --error=/data/users/srasch/assembly_course/Error/error_merqury_evaluation_%j.e
+#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/evaluation/merqury_logs/output_merqury_evaluation_%j.o
+#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/evaluation/merqury_logs/error_merqury_evaluation_%j.e
 #SBATCH --partition=pall
 
 ### Run this script 4 times.
@@ -17,17 +17,17 @@
 #4. assembly_name=flye; evaulation_dir=${polish_evaluation_dir}/evaluation_no_polish; meryl_dir=${polish_evaluation_dir}/evaluation/meryl; assembly=${course_dir}/02_assembly/flye/assembly.fasta
 
 #Specify name of assembly (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
-    assembly_name=canu
-    # assembly_name=flye
+    #assembly_name=canu
+    assembly_name=flye
 
 #Specify directory structure and create them
-    course_dir=/data/users/srasch/assembly_course
-        raw_data_dir=${course_dir}/RawData
-        polish_evaluation_dir=${course_dir}/03_polish_evaluation
-            evaulation_dir=${polish_evaluation_dir}/evaluation
-            # evaulation_dir=${polish_evaluation_dir}/evaluation_no_polish #Use this instead of the upper one when analysing the not polished assemblies
-                meryl_dir=${evaulation_dir}/meryl
-                # meryl_dir=${polish_evaluation_dir}/evaluation/meryl #Use this instead of the upper one when analysing the not polished assemblies
+    course_dir=/data/users/lbroennimann/assembly_annotation_course
+        raw_data_dir=${course_dir}/participant_3
+        polish_evaluation_dir=${course_dir}/assembly_validation/bowtie2/03_polish_evaluation
+            #evaulation_dir=${polish_evaluation_dir}/evaluation
+            evaulation_dir=${polish_evaluation_dir}/evaluation_no_polish #Use this instead of the upper one when analysing the not polished assemblies
+                #meryl_dir=${evaulation_dir}/meryl
+                meryl_dir=${polish_evaluation_dir}/evaluation/meryl #Use this instead of the upper one when analysing the not polished assemblies
                 merqury_dir=${evaulation_dir}/merqury
                     assembly_merqury_dir=${merqury_dir}/${assembly_name}
     
@@ -35,10 +35,10 @@
     mkdir ${assembly_merqury_dir}
 
 #Specify the assembly to use (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
-    assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta #Polished canu assembly
-    # assembly=${course_dir}/02_assembly/canu/canu.contigs.fasta #Unpolished canu assembly
-    # assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta #Polished flye assembly
-    # assembly=${course_dir}/02_assembly/flye/assembly.fasta #Unpolished flye assembly
+    #assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta #Polished canu assembly
+    #assembly=${course_dir}/assembly/canu/pacbio_assemblies/ERR3415819_ERR3415820_canu/ERR3415819.contigs.fasta #Unpolished canu assembly
+    #assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta #Polished flye assembly
+    assembly=${course_dir}/assembly/flye/pacbio_assemblies/ERR3415819_ERR3415820_assembly/assembly.fasta #Unpolished flye assembly
 
 #Change permisson of assembly otherwise there is an error (I did not fully understand why) and go to folder where results should be stored.
     chmod ugo+rwx ${assembly}
