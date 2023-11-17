d1560819b48aea4ed61d7e8746b90bf258d39f94
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2023-11-17 20:45:52 +0100

    fixed directories in genespace.R

--- genespace.R
+++ genespace.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(GENESPACE)
-gpar <- init_genespace(wd = "/data/users/lbroennimann/assembly_annotation_course/09_genespace", path2mcscanx = "/data/users/lbroennimann/assembly_annotation_course/MCScanX")
+# clone MCScanX from https://github.com/wyp1125/MCScanX.git
+gpar <- init_genespace(wd = "/data/users/lbroennimann/assembly_annotation_course/8_comparative_genomics", path2mcscanx = "/data/users/lbroennimann/assembly_annotation_course/MCScanX")
 out <- run_genespace(gsParam = gpar, overwrite = T)
 
 
