0e0b8e9ef7debd19d943e6ec3b4293db9652ec35
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2023-11-24 16:52:19 +0100

    made script more reproducible & fixed small bug

--- 01_01_get_read_statistics.sh
+++ 01_01_get_read_statistics.sh
@@ -1,49 +1,50 @@
 #!/usr/bin/env bash
 
-# Set environment variables and paths
-WORKING_DIR="/data/users/lbroennimann/assembly_annotation_course"
-OUTPUT_DIR="$WORKING_DIR/read_QC/fastqc"
-LOG_DIR="$OUTPUT_DIR/logs"
-PARTICIPANT_DIR="$WORKING_DIR/participant_3"
-EMAIL=lea.broennimann@students.unibe.ch
-
-# Create output and log directories if they don't exist
-mkdir -p "$OUTPUT_DIR/illumina"
-mkdir -p "$OUTPUT_DIR/pacbio"
-mkdir -p "$OUTPUT_DIR/RNAseq"
-mkdir -p "$LOG_DIR"
-
-# Define output and error log file paths
-OUTPUT_LOG="$LOG_DIR/output_fastqc_%j.o"
-ERROR_LOG="$LOG_DIR/error_fastqc_%j.e"
-
-# SBATCH parameters for job
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4000M
 #SBATCH --time=03:00:00
 #SBATCH --job-name=fastqc
-#SBATCH --mail-user=$EMAIL
+#SBATCH --mail-user=lea.broennimann@gmail.com
 #SBATCH --mail-type=begin,end
-#SBATCH --output=$OUTPUT_LOG
-#SBATCH --error=$ERROR_LOG
+#SBATCH --output=/data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/logs/output_fastqc_%j.o
+#SBATCH --error=/data/users/lbroennimann/assembly_annotation_course/read_QC/fastqc/logs/error_fastqc_%j.e
 #SBATCH --partition=pall
+
+# Usage: This script performs quality control checks using FastQC on fasta sequencing data.
+
+# Configuration
+WORKING_DIR=/data/users/lbroennimann/assembly_annotation_course
+OUTPUT_DIR=${WORKING_DIR}/read_QC/fastqc
+PARTICIPANT_DIR=${WORKING_DIR}/participant_3
+
+# Create output directories if they don't exist
+mkdir -p "$OUTPUT_DIR/illumina" "$OUTPUT_DIR/pacbio" "$OUTPUT_DIR/RNAseq"
 
 # Load FastQC module
 module load UHTS/Quality_control/fastqc/0.11.9
 
-# Perform FastQC on Illumina files
-cd "$PARTICIPANT_DIR/Illumina"
-fastqc -o "$OUTPUT_DIR/illumina" *fastq.gz
+# Function to perform FastQC
+perform_fastqc() {
+    local input_dir=$1
+    local output_dir=$2
+    local file_type=$3
 
-# Perform FastQC on PacBio files
-cd "$PARTICIPANT_DIR/pacbio"
-fastqc -o "$OUTPUT_DIR/pacbio" *fastq.gz
+    if [ ! -d "$input_dir" ]; then
+        echo "Directory $input_dir not found. Skipping FastQC for $file_type files."
+        return
+    fi
 
-# Perform FastQC on RNAseq files
-cd "$PARTICIPANT_DIR/RNAseq"
-fastqc -o "$OUTPUT_DIR/RNAseq" *fastq.gz
+    echo "Performing FastQC on $file_type files..."
+    cd "$input_dir"
+    fastqc -o "$output_dir" *fastq.gz
+}
 
+# Perform FastQC on different file types
+perform_fastqc ${PARTICIPANT_DIR}/Illumina ${OUTPUT_DIR}/illumina "Illumina"
+perform_fastqc ${PARTICIPANT_DIR}/pacbio ${OUTPUT_DIR}/pacbio "PacBio"
+perform_fastqc ${PARTICIPANT_DIR}/RNAseq ${OUTPUT_DIR}/RNAseq "RNAseq"
 
+echo "FastQC analysis completed."
 
 
 
